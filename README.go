8aafd21387cb887a78899bd41e1621c0f694b538
Valentin Matveychik <valentin.matveychik@faraway.gg>
2024-09-26 19:38:10 +0400

    init

---
+++ README.go
@@ -0,0 +1,5 @@
+# Currency Converter CLI
+
+Currency Converter CLI is a command-line utility that allows you to convert one currency into another. It is designed with a focus on extensibility and supports multiple data sources for exchange rates.
+
+Currently, **CoinMarketCap API** is used as the only data source, but this field is configurable, and additional API providers can be integrated in the future.
